8dd6fd1c71dd8daed003a5c9dddbf3069dec1bc2
Belén Albeza <belen@hey.com>
2024-12-14 16:22:39 +0100

    Add day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -4,6 +4,8 @@ use regex::Regex;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Op {
+    Enable,
+    Disable,
     Mul(u32, u32),
 }
 
@@ -17,7 +19,7 @@ impl Parser {
         Self { src: input }
     }
 
-    fn parse(&self) -> Vec<Op> {
+    fn parse_part1(&self) -> Vec<Op> {
         lazy_static! {
             static ref RE: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
         }
@@ -27,11 +29,53 @@ impl Parser {
             .map(|(_, [a, b])| Op::Mul(a.parse().unwrap(), b.parse().unwrap()))
             .collect()
     }
+
+    fn parse_part2(&self) -> Vec<Op> {
+        lazy_static! {
+            static ref RE_MUL: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+            static ref RE_DO: Regex = Regex::new(r"do\(\)").unwrap();
+            static ref RE_DONT: Regex = Regex::new(r"don\'t\(\)").unwrap();
+        }
+
+        let muls: Vec<(usize, Op)> = RE_MUL
+            .captures_iter(&self.src)
+            .map(|c| (c.get(0).unwrap().start(), c.extract()))
+            .map(|(index, (_, [a, b]))| {
+                let op = Op::Mul(a.parse().unwrap(), b.parse().unwrap());
+                (index, op)
+            })
+            .collect();
+        let dos: Vec<(usize, Op)> = RE_DO
+            .captures_iter(&self.src)
+            .map(|c| {
+                let index = c.get(0).unwrap().start();
+                (index, Op::Enable)
+            })
+            .collect();
+
+        let donts: Vec<(usize, Op)> = RE_DONT
+            .captures_iter(&self.src)
+            .map(|c| {
+                let index = c.get(0).unwrap().start();
+                (index, Op::Disable)
+            })
+            .collect();
+
+        let mut all = vec![muls, dos, donts].concat();
+        all.sort_by_key(|(index, _)| *index);
+
+        all.into_iter().map(|(_, op)| op).collect()
+    }
 }
 
-#[aoc_generator(day3)]
-fn parse(input: &str) -> Vec<Op> {
-    Parser::new(input.to_owned()).parse()
+#[aoc_generator(day3, part1)]
+fn parse_part1(input: &str) -> Vec<Op> {
+    Parser::new(input.to_owned()).parse_part1()
+}
+
+#[aoc_generator(day3, part2)]
+fn parse_part2(input: &str) -> Vec<Op> {
+    Parser::new(input.to_owned()).parse_part2()
 }
 
 #[aoc(day3, part1)]
@@ -40,19 +84,43 @@ fn part1(input: &[Op]) -> u32 {
         .into_iter()
         .map(|op| match op {
             Op::Mul(a, b) => a * b,
+            _ => unreachable!("not available on part 1"),
         })
         .sum()
+}
+
+#[aoc(day3, part2)]
+fn part2(input: &[Op]) -> u32 {
+    let mut res = 0;
+    let mut enabled = true;
+
+    for &op in input {
+        match op {
+            Op::Enable => {
+                enabled = true;
+            }
+            Op::Mul(a, b) if enabled => res += a * b,
+            Op::Disable => {
+                enabled = false;
+            }
+            _ => {}
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT1: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn part1_parse_input() {
-        let res = Parser::new(INPUT.to_owned()).parse();
+        let res = Parser::new(INPUT1.to_owned()).parse_part1();
         assert_eq!(
             res,
             vec![Op::Mul(2, 4), Op::Mul(5, 5), Op::Mul(11, 8), Op::Mul(8, 5)]
@@ -61,6 +129,27 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(INPUT)), 161);
+        assert_eq!(part1(&parse_part1(INPUT1)), 161);
+    }
+
+    #[test]
+    fn part2_parse_input() {
+        let res = Parser::new(INPUT2.to_owned()).parse_part2();
+        assert_eq!(
+            res,
+            vec![
+                Op::Mul(2, 4),
+                Op::Disable,
+                Op::Mul(5, 5),
+                Op::Mul(11, 8),
+                Op::Enable,
+                Op::Mul(8, 5)
+            ]
+        )
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse_part2(INPUT2)), 48);
     }
 }
